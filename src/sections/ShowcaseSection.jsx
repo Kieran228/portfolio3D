9153e564fa1bc86e229aad8dbe4d1a3494bd5401
Kieran Caraway <154688088+Kieran228@users.noreply.github.com>
2025-05-07 14:47:03 -0500

    updated showcaseSection transititons using gsap plugins

--- src/sections/ShowcaseSection.jsx
+++ src/sections/ShowcaseSection.jsx
@@ -6,20 +6,45 @@ import { ScrollTrigger } from "gsap/ScrollTrigger";
 
 gsap.registerPlugin(ScrollTrigger);
 
-
 const ShowcaseSection = () => {
-    const sectionRef = useRef(null);
-    const project1Ref = useRef(null);
-    const project2Ref = useRef(null);
-    const project3Ref = useRef(null);
-    
-    useGSAP(() => {
-      gsap.fromTo(
-        sectionRef.current,
-        { opacity: 0 },
-        { opacity: 1, duration: 1.5 }
-      );
-    }, []);
+  const sectionRef = useRef(null);
+  const project1Ref = useRef(null);
+  const project2Ref = useRef(null);
+  const project3Ref = useRef(null);
+
+  useGSAP(() => {
+      const projects = [
+        project1Ref.current,
+        project2Ref.current,
+        project3Ref.current,
+      ];
+
+      projects.forEach((card, index) => {
+        gsap.fromTo(
+          card,
+          {
+            y: 50,
+            opacity: 0,
+          },
+          {
+            y: 0,
+            opacity: 1,
+            duration: 1,
+            delay: 0.3 * (index + 1),
+            scrollTrigger: {
+              trigger: card,
+              start: "top bottom-=100",
+            }
+          }
+        );
+      });
+
+    gsap.fromTo(
+      sectionRef.current,
+      { opacity: 0 },
+      { opacity: 1, duration: 1.5 }
+    );
+  }, []);
 
   return (
     <section id="work" ref={sectionRef} className="app-showcase">
